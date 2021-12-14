3824f00a1d7d18037cf8f543593c2f2988c28fb8
Martin <martinwang2002@outlook.com>
2021-12-14 14:56:21 +0000

    feat(page): placebo progress bar
    placebo progress bar; fixed Header; eslint rules.

--- src/components/page/page.tsx
+++ src/components/page/page.tsx
@@ -1,20 +1,109 @@
 import React from "react"
 import Header from "./header"
+import type { NextRouter } from "next/router"
+import { withRouter } from "next/router"
 
 import style from "./page.module.scss"
 
 interface PageProps{
   children: React.ReactNode
+  router: NextRouter
 }
 
-class Page extends React.PureComponent<PageProps> {
+interface PageState {
+  indeterminate: boolean
+  progress: number
+}
+
+class Page extends React.PureComponent<PageProps, PageState> {
+  public constructor (props: PageProps | Readonly<PageProps>) {
+    super(props)
+    this.state = {
+      indeterminate: false,
+      progress: 0
+    }
+    // prevent this.state undefined error
+    this.handleTransitionEnd = this.handleTransitionEnd.bind(this)
+  }
+
+  public componentDidMount (): void {
+    // register router event listeners
+    const { router } = this.props
+    router.events.on("routeChangeStart", () => {
+      this.handleStart()
+    })
+    router.events.on("routeChangeComplete", () => {
+      this.handleStop()
+    })
+    router.events.on("routeChangeError", () => {
+      this.handleStop()
+    })
+  }
+
+  public componentWillUnmount (): void {
+    // unregister router event listeners
+    const { router } = this.props
+    router.events.off("routeChangeStart", () => {
+      this.handleStart()
+    })
+    router.events.off("routeChangeComplete", () => {
+      this.handleStop()
+    })
+    router.events.off("routeChangeError", () => {
+      this.handleStop()
+    })
+  }
+
+  private handleStart (): void {
+    this.setState({
+      indeterminate: true,
+      progress: 80
+    })
+  }
+
+  private handleStop (): void {
+    this.setState({
+      indeterminate: false,
+      progress: 100
+    })
+  }
+
+  public handleTransitionEnd (): void {
+    const progress100 = 100
+    const { progress } = this.state
+    if (progress === progress100) {
+      this.setState({
+        progress: 0
+      })
+    }
+  }
+
   public render (): React.ReactNode {
     const { children } = this.props
+    const { indeterminate, progress } = this.state
+    const progress0 = 0
+    const opacity0 = 0
+    const opacity1 = 1
     return (
       <>
+        <div className={style.placebo}>
+          <div
+            aria-valuemax={100}
+            aria-valuemin={0}
+            aria-valuenow={progress}
+            className={style.placebo_bar}
+            onTransitionEnd={this.handleTransitionEnd}
+            role="progressbar"
+            style={{
+              width: String(progress) + "%",
+              opacity: progress === progress0 ? opacity0 : opacity1
+            }}
+          />
+        </div>
+
         <Header />
 
-        <main className={style["container-lg"]}>
+        <main className={!indeterminate ? style.main_container : style.main_container_fadeout}>
           {children}
         </main>
       </>
@@ -23,4 +112,4 @@ class Page extends React.PureComponent<PageProps> {
   }
 }
 
-export default Page
+export default withRouter(Page)
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import "./styles.scss"
 
 // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types, @typescript-eslint/naming-convention
 function myApp ({ Component, pageProps }: AppProps): React.ReactChild {
+  // eslint-disable-next-line react/jsx-props-no-spreading
   return <Component {...pageProps} />
 }
 
