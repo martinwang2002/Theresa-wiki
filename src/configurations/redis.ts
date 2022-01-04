8ee14e8b481ba7b4cfb6ede6c549ce800b1c1a42
Martin <martinwang2002@outlook.com>
2022-01-04 20:27:31 +0000

    feat(redis configurations): redis cacheable
    cacheable wrapper function to use redis cache based on cacheKey

--- src/configurations/redis.ts
+++ src/configurations/redis.ts
@@ -1,9 +1,16 @@
 import Redis from "ioredis"
 
+interface ICachable {
+  cacheKey: string
+  hashKey?: string
+  expiryMode?: string | undefined
+  ttl?: number | undefined
+  setMode?: number | string | undefined
+}
+
 const redisConfig: Redis.RedisOptions = {
   autoResubscribe: false,
-  lazyConnect: true,
-  maxRetriesPerRequest: 0
+  lazyConnect: true
 }
 
 const redisClient = new Redis(process.env.REDIS_URL, redisConfig)
@@ -18,4 +25,27 @@ if (process.env.REDIS_URL === "") {
   })
 }
 
-export default redisClient
+const cacheable = <T extends unknown[], U>(fn: (...args: T) => Promise<U> | U, options: Readonly<ICachable>) => {
+  return async (...args: T): Promise<U> => {
+    try {
+      let result: U
+      const cacheKey = options.cacheKey + args.join("_")
+      result = await redisClient.get(cacheKey).then(async (redisResult) => {
+        if (redisResult != null) {
+          return JSON.parse(redisResult) as U
+        } else {
+          result = await fn(...args)
+          await redisClient.set(cacheKey, JSON.stringify(result), options.expiryMode, options.ttl, options.setMode)
+          return result
+        }
+      })
+      return result
+    } catch (error) {
+      // TODO: Record error
+      return fn(...args)
+    }
+  }
+}
+
+export default cacheable
+export { redisClient }
--- src/models/gamedata/excel/stage.ts
+++ src/models/gamedata/excel/stage.ts
@@ -1,4 +1,4 @@
-import redisClient from "@/configurations/redis"
+import cacheable from "@/configurations/redis"
 
 interface IStageTable {
   [key: string]: unknown
@@ -41,31 +41,12 @@ interface IMapData {
   width: number
 }
 
-const fetchStageTable = async (): Promise<IStageTable> => {
+const stageTable = cacheable(async (): Promise<IStageTable> => {
   const url = "https://s3-torappu.martinwang2002.com/api/v0/CN/Android/latest/unpacked_assetbundle/assets/torappu/dynamicassets/gamedata/excel/stage_table.json"
   const stageTableRes = await fetch(url)
   const stageTableJson = await stageTableRes.json() as IStageTable
   return stageTableJson
-}
-
-const stageTable = async (): Promise<IStageTable> => {
-  let stageTableResult
-  try {
-    stageTableResult = await redisClient.get("stageTable").then(async (result) => {
-      if (result != null) {
-        return JSON.parse(String(result)) as IStageTable
-      } else {
-        stageTableResult = await fetchStageTable()
-        const expiration = 3600
-        await redisClient.set("stageTable", JSON.stringify(stageTable), "EX", expiration)
-        return stageTableResult
-      }
-    })
-  } catch {
-    stageTableResult = await fetchStageTable()
-  }
-  return stageTableResult
-}
+}, { cacheKey: "stageTable", expiryMode: "EX", ttl: 3600 })
 
 export const stageIdtoLodash = (stageId: string): string => {
   return stageId.replaceAll("#", "__")
