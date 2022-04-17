186afe5b997847a26ebb24543ff881b635cc225e
Martin <martinwang2002@outlook.com>
2022-04-17 18:30:05 +0000

    feat(map): improved display zone name
    improve display zone name in map

--- src/models/utils/getDisplayZoneName.ts
+++ src/models/utils/getDisplayZoneName.ts
@@ -1,9 +1,14 @@
 import type { IZoneInfo } from "@/models/gamedata/excel/zoneTable"
 
 export const getDisplayZoneName = (zoneInfo: Readonly<IZoneInfo>): string => {
-  if (zoneInfo.zoneNameFirst !== null) {
-    return `${zoneInfo.zoneNameFirst} - ${zoneInfo.zoneNameSecond}`
+  const zoneNames = [zoneInfo.zoneNameFirst, zoneInfo.zoneNameSecond, zoneInfo.zoneNameThird]
+
+  // nonNullZoneNames is not empty
+  const nonNullZoneNames = zoneNames.filter(name => name !== null)
+
+  if (nonNullZoneNames.length) {
+    return nonNullZoneNames.join(" - ")
   } else {
-    return zoneInfo.zoneNameSecond
+    return zoneInfo.zoneID
   }
 }
