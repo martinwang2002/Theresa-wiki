cdfb8e861ef850d30f5bd10fc39779fc6e0172da
Martin <martinwang2002@outlook.com>
2022-06-04 12:28:48 +0000

    feat(about): index page

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,30 @@
+import React from "react"
+
+import Head from "next/head"
+
+import Page from "@/components/page/page"
+
+export default class Privacy extends React.PureComponent {
+  public render (): React.ReactNode {
+    return (
+      <Page>
+        <Head>
+
+          <title>
+            关于本站 | Theresa.wiki
+          </title>
+
+        </Head>
+
+        <div style={{ overflow: "hidden", marginTop: "1rem" }}>
+          在页面底部，有很多连接哦~
+
+          <br />
+
+          摆烂.jpg
+        </div>
+
+      </Page>
+    )
+  }
+}
