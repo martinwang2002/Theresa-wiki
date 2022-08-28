6a4842f7c9efecf6ffbfecd12f5bb501fe030d17
Martin <martinwang2002@outlook.com>
2022-08-28 22:17:47 +0000

    fix: server runtime config use process.env

--- src/configurations/runtimeConfig.ts
+++ src/configurations/runtimeConfig.ts
@@ -1,28 +1,33 @@
 import getConfig from "next/config"
+import { parse as parseUri } from "uri-js"
 import type { URIComponents } from "uri-js"
 
 interface IPublicRuntimeConfig {
-  /* eslint-disable @typescript-eslint/naming-convention */
   GIT_COMMIT: string
   GTAG_ID: string
   THERESA_STATIC: URIComponents
   THERESA_WIKI_VERSION: string
   CRISP_WEBSITE_ID: string
-  /* eslint-enable @typescript-eslint/naming-convention */
 }
 
 interface IServerRuntimeConfig {
-  /* eslint-disable @typescript-eslint/naming-convention */
   NO_DYNAMIC_ROUTES: boolean
   REDIS_EX_TTL: number
   REDIS_URL: string
   THERESA_S3: URIComponents
-  /* eslint-enable @typescript-eslint/naming-convention */
 }
 
-const { publicRuntimeConfig, serverRuntimeConfig } = getConfig() as {
+const { publicRuntimeConfig } = getConfig() as {
   publicRuntimeConfig: IPublicRuntimeConfig
-  serverRuntimeConfig: IServerRuntimeConfig
 }
+
+const defaultRedisExTtl = 3600
+
+const serverRuntimeConfig = {
+  NO_DYNAMIC_ROUTES: process.env.NODE_ENV === "development" || process.env.THERESA_WIKI_NO_BUILD_DYNAMIC_ROUTES?.toLowerCase() === "true",
+  REDIS_EX_TTL: parseInt(process.env.REDIS_EX_TTL ?? "") || defaultRedisExTtl,
+  REDIS_URL: process.env.REDIS_URL ?? "",
+  THERESA_S3: parseUri(process.env.THERESA_S3 ?? "http://s3.theresa.wiki")
+} as IServerRuntimeConfig
 
 export { publicRuntimeConfig, serverRuntimeConfig }
