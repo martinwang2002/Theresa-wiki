4af5e17b2257010646b5fee50361ba1ec2311fdf
Martin <martinwang2002@outlook.com>
2022-10-02 13:02:55 +0000

    feat(map): tile tile_telin and tile_telout

--- src/components/map/scene/tile/tileSvglike.tsx
+++ src/components/map/scene/tile/tileSvglike.tsx
@@ -2,6 +2,9 @@
 /* eslint-disable react/no-multi-comp */
 import React from "react"
 
+import LoginIcon from "@mui/icons-material/Login"
+import LogoutIcon from "@mui/icons-material/Logout"
+import { lightBlue } from "@mui/material/colors"
 import type { SxProps, Theme } from "@mui/system"
 
 import TileFlystartIcon from "@/components/icon/tile/tileFlystart"
@@ -54,4 +57,38 @@ const TileEnd = React.forwardRef<HTMLSpanElement>((props, ref) => (
 
 TileEnd.displayName = "TileEnd"
 
-export { TileFlystart, TileStart, TileEnd }
+const TileTelin = React.forwardRef<HTMLSpanElement>((props, ref) => (
+  <TileBase
+    ref={ref}
+    {...props}
+  >
+    <LoginIcon
+      sx={{
+        ...tileSxProps,
+        color: (theme) => theme.palette.mode === "light" ? lightBlue[600] : lightBlue[300],
+        padding: "12.5%"
+      } as SxProps<Theme>}
+    />
+  </TileBase>
+))
+
+TileTelin.displayName = "TileTelout"
+
+const TileTelout = React.forwardRef<HTMLSpanElement>((props, ref) => (
+  <TileBase
+    ref={ref}
+    {...props}
+  >
+    <LogoutIcon
+      sx={{
+        ...tileSxProps,
+        color: (theme) => theme.palette.mode === "light" ? lightBlue[600] : lightBlue[300],
+        padding: "12.5%"
+      } as SxProps<Theme>}
+    />
+  </TileBase>
+))
+
+TileTelout.displayName = "TileTelout"
+
+export { TileFlystart, TileStart, TileEnd, TileTelin, TileTelout }
