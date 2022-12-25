ff0ecfb5dc400f8be14bc2ced6e2cc433076cb1a
Martin <martinwang2002@outlook.com>
2022-12-25 17:55:44 +0000

    fix(map): preview image size

--- src/components/map/mapPreview/mapPreviewImage.tsx
+++ src/components/map/mapPreview/mapPreviewImage.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 
 import Image from "next/image"
-import type { ImageLoaderProps } from "next/image"
 import { serialize } from "uri-js"
 
 import { publicRuntimeConfig } from "@/configurations/runtimeConfig"
@@ -10,27 +9,24 @@ interface MapPreviewImageProps {
   stageId: string
 }
 
-// eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
-const staticLoader = ({ src, width, quality }: Readonly<ImageLoaderProps>): string => {
-  const fallbackQuality = 75
-  return src.replace("width", String(width)).replace("quality", String(quality ?? fallbackQuality))
-}
-
 class MapPreviewImage extends React.PureComponent<MapPreviewImageProps> {
   public render (): React.ReactNode {
     const { stageId } = this.props
 
     const normalStageId = stageId.replace("__f__", "")
+
+    const width = 1080
+    const quality = 75
+
     const imageSrc = serialize({
       ...publicRuntimeConfig.THERESA_STATIC,
-      path: `/api/v0/AK/CN/Android/mappreview/${normalStageId}/width/quality`
+      path: `/api/v0/AK/CN/Android/mappreview/${normalStageId}/${width}/${quality}`
     })
 
     return (
       <Image
         alt={`Stage Preview Picture ${stageId}`}
         fill
-        loader={staticLoader}
         src={imageSrc}
       />
     )
