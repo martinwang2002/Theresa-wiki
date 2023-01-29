17b385139c47ffe2e63ef81df2a021c49d9c94dd
Martin <martinwang2002@outlook.com>
2023-01-29 15:50:42 +0000

    fix(api): startup script access

--- src/pages/api/startup.ts
+++ src/pages/api/startup.ts
@@ -5,11 +5,9 @@ import { addRevalidatePaths } from "./health"
 
 // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
 const startup = (req: NextApiRequest, res: NextApiResponse): void => {
-  if (req.socket.remoteAddress?.startsWith("127.") || req.socket.remoteAddress?.startsWith("::ffff:127.")) {
-    addRevalidatePaths().catch((reason) => {
-      console.warn("failed to add revalidate paths", reason)
-    })
-  }
+  addRevalidatePaths().catch((reason) => {
+    console.warn("failed to add revalidate paths", reason)
+  })
 
   res.status(StatusCodes.OK).json({
     health: "ok"
